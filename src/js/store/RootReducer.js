2c81684343486171a59d2da1dba8cec7b5af6a5c
michealabid <michealabidfekry@gmail.com>
2021-01-11 14:54:19 +0200

    add-switch-cases-insteadof-if-statments

--- src/js/store/RootReducer.js
+++ src/js/store/RootReducer.js
@@ -3,22 +3,25 @@ import { DATA_LOADED_AGAIN, GET_USERS_FAILED } from "./action-types";
 const initialState = {
   // articles: [],
   remoteMovies: [],
-  error: null
+  error: null,
 };
 
 function rootReducer(state = initialState, action) {
-  if (action.type === DATA_LOADED_AGAIN) {
-    return Object.assign({}, state, {
-      remoteMovies: action.movies.items,
-    });
+  switch (action.type) {
+    case DATA_LOADED_AGAIN:
+      return Object.assign({}, state, {
+        remoteMovies: action.movies.items,
+      });
+
+    case GET_USERS_FAILED:
+      return Object.assign({}, state, {
+        remoteMovies: [],
+        error: action.message,
+      });
+
+    default:
+      return state;
   }
-  if (action.type === GET_USERS_FAILED) {
-    return Object.assign({}, state, {
-      remoteMovies: [],
-      error: action.message
-    });
-  }
-  return state;
 }
 
 export default rootReducer;
