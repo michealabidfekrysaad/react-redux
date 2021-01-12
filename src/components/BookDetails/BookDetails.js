bc51446d94183cb4d9189bdfc5f653a1243b649d
michealabid <michealabidfekry@gmail.com>
2021-01-12 17:34:54 +0200

    make image background not img tag due to stretching

--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -5,24 +5,27 @@ const BookDetails = ({ authors, imageLinks, description, previewLink }) => {
   return (
     //   ana kont 7atet el key hena w dah 3'alat
     // el mafrod yeb2a fe el parent ta7t el map ya stupid
-    <div className="card">
-      <img
-        src={
-          imageLinks ? imageLinks.thumbnail : "https://via.placeholder.com/150"
-        }
-        alt="Avatar"
+    <section>
+      <div
+        className="bg-image"
+        style={{
+          backgroundImage: imageLinks ? `url(${imageLinks.thumbnail})` : `url(https://via.placeholder.com/150)`  ,
+        }}
+        
       />
-      <a href={previewLink ? previewLink : "https://www.google.com"}>
-        <div className="container">
-          <h4 className="truncate">
-            <b>{authors ? authors[0]: "no author"}</b>
-          </h4>
-          <p className="truncate">
-            {description ? description : "it is very nice book"}
-          </p>
-        </div>
-      </a>
-    </div>
+      <div className="card">
+        <a href={previewLink ? previewLink : "https://www.google.com"}>
+          <div className="container">
+            <h4 className="truncate">
+              <b>{authors ? authors[0] : "no author"}</b>
+            </h4>
+            <p className="truncate">
+              {description ? description : "it is very nice book"}
+            </p>
+          </div>
+        </a>
+      </div>
+    </section>
   );
 };
 
